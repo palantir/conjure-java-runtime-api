720df861d020cdb8bfa833a39cad7b7219df2ffc
Robert Fink <rfink@palantir.com>
2017-08-14 17:34:53 -0400

    Ben comments: remove Retry-After

--- errors/src/main/java/com/palantir/remoting/api/errors/QosException.java
+++ errors/src/main/java/com/palantir/remoting/api/errors/QosException.java
@@ -20,11 +20,8 @@ import com.palantir.logsafe.Arg;
 import com.palantir.logsafe.SafeArg;
 import com.palantir.logsafe.SafeLoggable;
 import java.net.URL;
-import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * An exception raised by a service to indicate a potential Quality-of-Service problem, specifically requesting that the
@@ -34,66 +31,50 @@ import java.util.Optional;
  */
 public class QosException extends RuntimeException {
 
-    // Not meant for subclassing outside this class.
+    // Not meant for external subclassing.
     private QosException() {}
 
     /**
-     * Returns a {@link Retry} exception indicating that the calling client should retry the request without delay and
-     * against an arbitrary node of this service.
+     * Returns a {@link Retry} exception indicating that the calling client should retry the request against an
+     * arbitrary node of this service.
      */
-    public static Retry retryNow() {
-        return new Retry(Optional.empty(), Optional.empty());
+    public static Retry retry() {
+        return new Retry();
     }
 
     /**
-     * Returns a {@link Retry} exception indicating that the calling client should retry the given minimum delay and
-     * against an arbitrary node of this service.
+     * Returns a {@link RetryOther} exception indicating that the calling client should retry against the given node of
+     * this service.
      */
-    public static Retry retryLater(Duration backoff) {
-        return new Retry(Optional.empty(), Optional.of(backoff));
-
+    public static RetryOther retryOther(URL redirectTo) {
+        return new RetryOther(redirectTo);
     }
 
     /**
-     * Returns a {@link Retry} exception indicating that the calling client should retry the request without delay and
-     * against the given node of this service.
-     */
-    public static Retry retryOther(URL redirectTo) {
-        return new Retry(Optional.of(redirectTo), Optional.empty());
-
-    }
-
-    /**
-     * See {@link Unavailable}.
+     * An exception indicating that all nodes of this service are currently unavailable and the client shall not attempt
+     * to wait for it to become available again. Typically, human intervention may be required to bring this service
+     * back up.
      */
     public static Unavailable unavailable() {
         return new Unavailable();
     }
 
-    /**
-     * An exception indicating that a request against this service may be retried, potentially against a different node
-     * of this service and with a given delay.
-     */
-    public static final class Retry extends QosException implements SafeLoggable {
-        private final Optional<URL> redirectTo;
-        private final Optional<Duration> backoff;
+    /** See {@link #retry}. */
+    public static final class Retry extends QosException {
+        private Retry() {}
+    }
 
-        private Retry(Optional<URL> redirectTo, Optional<Duration> backoff) {
+    /** See {@link #retryOther}. */
+    public static final class RetryOther extends QosException implements SafeLoggable {
+        private final URL redirectTo;
+
+        private RetryOther(URL redirectTo) {
             this.redirectTo = redirectTo;
-            this.backoff = backoff;
         }
 
-        /**
-         * If non-empty, indicates an alternative URL of this service against which the request may be retried. If
-         * empty, indicates that an arbitrary node may be used for the retry.
-         */
-        public Optional<URL> getRedirectTo() {
+        /** Indicates an alternative URL of this service against which the request may be retried. */
+        public URL getRedirectTo() {
             return redirectTo;
-        }
-
-        /** If non-empty, indicates the minimum suggest delay/backoff time before the request is retried. */
-        public Optional<Duration> getBackoff() {
-            return backoff;
         }
 
         @Override
@@ -104,27 +85,13 @@ public class QosException extends RuntimeException {
         @Override
         public List<Arg<?>> getArgs() {
             List<Arg<?>> args = new ArrayList<>();
-            redirectTo.ifPresent(r -> args.add(SafeArg.of("redirectTo", r)));
-            backoff.ifPresent(b -> args.add(SafeArg.of("backoff", b)));
+            args.add(SafeArg.of("redirectTo", redirectTo));
             return args;
         }
     }
 
-    /**
-     * An exception indicating that all nodes of this service are currently unavailable and the client shall not attempt
-     * to wait for it to become available again. Typically, human intervention may be required to bring this service
-     * back up.
-     */
-    public static final class Unavailable extends QosException implements SafeLoggable {
-
-        @Override
-        public String getLogMessage() {
-            return this.getClass().getSimpleName() + ": Service unavailable";
-        }
-
-        @Override
-        public List<Arg<?>> getArgs() {
-            return Collections.emptyList();
-        }
+    /** See {@link #unavailable}. */
+    public static final class Unavailable extends QosException {
+        private Unavailable() {}
     }
 }
