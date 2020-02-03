palantir/conjure-java-runtime-api

commit 988d08408fafe71d09ca535fbf2fa8efcf1f75fc

Improvement: Only require keystore password for jks stores (#427)
Don't require passwords for non jks keystores

ssl-config/src/main/java/com/palantir/conjure/java/api/config/ssl/SslConfiguration.java
@@ -67,9 +67,9 @@ public abstract class SslConfiguration {
 
     @Value.Check
     protected final void check() {
-        if (keyStorePath().isPresent() != keyStorePassword().isPresent()) {
+        if (keyStorePath().isPresent() && keyStoreType().equals(StoreType.JKS) && !keyStorePassword().isPresent()) {
             throw new SafeIllegalArgumentException(
-                    "keyStorePath and keyStorePassword must both be present or both be absent");
+                    "keyStorePassword must be present if keyStoreType is JKS");
         }
         if (keyStoreKeyAlias().isPresent() && !keyStorePath().isPresent()) {
             throw new SafeIllegalArgumentException(

ssl-config/src/test/java/com/palantir/conjure/java/api/config/ssl/SslConfigurationTest.java
@@ -17,9 +17,12 @@
 package com.palantir.conjure.java.api.config.ssl;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.palantir.conjure.java.api.ext.jackson.ObjectMappers;
+import com.palantir.logsafe.exceptions.SafeIllegalArgumentException;
 import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 
@@ -67,5 +70,25 @@ public final class SslConfigurationTest {
                 .isEqualTo(serialized);
         assertThat(MAPPER.readValue(deserializedKebabCase, SslConfiguration.class))
                 .isEqualTo(serialized);
+    }
+
+    @Test
+    public void jksKeystorePassword() {
+        assertThatThrownBy(() -> SslConfiguration.builder()
+                .trustStorePath(Paths.get("truststore.jks"))
+                .keyStorePath(Paths.get("keystore.jks"))
+                .keyStoreType(SslConfiguration.StoreType.JKS)
+                .build())
+                        .isInstanceOf(SafeIllegalArgumentException.class)
+                        .hasMessage("keyStorePassword must be present if keyStoreType is JKS");
+    }
+
+    @Test
+    public void nonJksKeystorePassword() {
+        assertThatCode(() -> SslConfiguration.builder()
+                .trustStorePath(Paths.get("truststore.jks"))
+                .keyStorePath(Paths.get("key.pem"))
+                .keyStoreType(SslConfiguration.StoreType.PEM)
+                .build()).doesNotThrowAnyException();
     }
 }

commit 247fd1c77ffd6d91d5b48c25d8620a3fa13ab350

SslConfiguration validates that keyStorePassword requires keyStorePath
Fix SslConfiguration validation regression introduced by #427

ssl-config/src/main/java/com/palantir/conjure/java/api/config/ssl/SslConfiguration.java
@@ -76,6 +76,9 @@ public abstract class SslConfiguration {
 
     @Value.Check
     protected final void check() {
+        if (keyStorePassword().isPresent() && !keyStorePath().isPresent()) {
+            throw new SafeIllegalArgumentException("keyStorePath must be present if a keyStorePassword is provided");
+        }
         if (keyStorePath().isPresent()
                 && keyStoreType().equals(StoreType.JKS)
                 && !keyStorePassword().isPresent()) {