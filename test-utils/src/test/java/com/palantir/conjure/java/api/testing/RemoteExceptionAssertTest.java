c62de86c22dcfc60b5ec9bff715665e575bddeae
Jeff Martin <jmartin@palantir.com>
2023-11-21 12:23:52 -0500

    oops

--- test-utils/src/test/java/com/palantir/conjure/java/api/testing/RemoteExceptionAssertTest.java
+++ test-utils/src/test/java/com/palantir/conjure/java/api/testing/RemoteExceptionAssertTest.java
@@ -41,7 +41,7 @@ public final class RemoteExceptionAssertTest {
                         "Expected error status to be %s, but found %s",
                         actualType.httpErrorCode(), actualType.httpErrorCode() + 1)
                 // Make sure the error type was captured.
-                .hasMessageContaining("bla")
+                .hasMessageContaining("FAILED_PRECONDITION")
                 // Make sure the instance ID was captured.
                 .hasMessageContaining("with instance ID");
     }
