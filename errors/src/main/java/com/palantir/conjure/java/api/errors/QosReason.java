bea049906f976e84ca5d09209d81da0eb679d2a8
Carter Kozak <ckozak@palantir.com>
2024-10-03 16:52:25 -0400

    Initial implementation of qos metadata

--- errors/src/main/java/com/palantir/conjure/java/api/errors/QosReason.java
+++ errors/src/main/java/com/palantir/conjure/java/api/errors/QosReason.java
@@ -18,9 +18,13 @@ package com.palantir.conjure.java.api.errors;
 
 import com.google.errorprone.annotations.CompileTimeConstant;
 import com.palantir.logsafe.Preconditions;
+import com.palantir.logsafe.Safe;
 import com.palantir.logsafe.SafeArg;
+import com.palantir.logsafe.exceptions.SafeIllegalArgumentException;
 import java.util.Objects;
-import java.util.regex.Pattern;
+import java.util.Optional;
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nullable;
 
 /**
  * A class representing the reason why a {@link QosException} was created.
@@ -30,28 +34,43 @@ import java.util.regex.Pattern;
  * tag, for observability into {@link QosException} calls. As such, the string is constrained to have at most 50
  * lowercase alphanumeric characters, and hyphens (-).
  */
+@Safe
 public final class QosReason {
 
-    @CompileTimeConstant
+    @Safe
     private final String reason;
 
-    private static final String PATTERN_STRING = "^[a-z0-9\\-]{1,50}$";
-    private static final Pattern PATTERN = Pattern.compile(PATTERN_STRING);
+    private final Optional<RetryHint> retryHint;
+    private final Optional<DueTo> dueTo;
 
-    private QosReason(@CompileTimeConstant String reason) {
+    private static final String PATTERN_STRING = "^[a-z0-9\\-]{1,50}$";
+
+    private QosReason(@Safe String reason, Optional<RetryHint> retryHint, Optional<DueTo> dueTo) {
+        checkReason(reason);
         this.reason = reason;
+        this.retryHint = retryHint;
+        this.dueTo = dueTo;
     }
 
     public static QosReason of(
-            @CompileTimeConstant @org.intellij.lang.annotations.Pattern(PATTERN_STRING) String reason) {
-        Preconditions.checkArgument(
-                PATTERN.matcher(reason).matches(),
-                "Reason must be at most 50 characters, and only contain lowercase letters, numbers, "
-                        + "and hyphens (-).",
-                SafeArg.of("reason", reason));
-        return new QosReason(reason);
+            @Safe @CompileTimeConstant @org.intellij.lang.annotations.Pattern(PATTERN_STRING) String reason) {
+        return new QosReason(reason, Optional.empty(), Optional.empty());
     }
 
+    @Safe
+    public String reason() {
+        return reason;
+    }
+
+    public Optional<RetryHint> retryHint() {
+        return retryHint;
+    }
+
+    public Optional<DueTo> dueTo() {
+        return dueTo;
+    }
+
+    /** Returns the {@link #reason()} for historical reasons, and should not be updated. */
     @Override
     public String toString() {
         return reason;
@@ -61,16 +80,121 @@ public final class QosReason {
     public boolean equals(Object other) {
         if (other == this) {
             return true;
-        } else if (!(other instanceof QosReason)) {
-            return false;
+        } else if (other instanceof QosReason otherReason) {
+            return Objects.equals(this.reason, otherReason.reason)
+                    && Objects.equals(this.retryHint, otherReason.retryHint)
+                    && Objects.equals(this.dueTo, otherReason.dueTo);
         } else {
-            QosReason otherReason = (QosReason) other;
-            return Objects.equals(this.reason, otherReason.reason);
+            return false;
         }
     }
 
     @Override
     public int hashCode() {
         return Objects.hashCode(this.reason);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        @Nullable
+        private String reason;
+
+        @Nullable
+        private RetryHint retryHint;
+
+        @Nullable
+        private DueTo dueTo;
+
+        private Builder() {}
+
+        public Builder from(QosReason other) {
+            return reason(other.reason()).retryHint(other.retryHint()).dueTo(other.dueTo());
+        }
+
+        public Builder reason(
+                @Safe @CompileTimeConstant @org.intellij.lang.annotations.Pattern(PATTERN_STRING) String value) {
+            this.reason = Preconditions.checkNotNull(value, "reason");
+            return this;
+        }
+
+        public Builder retryHint(RetryHint value) {
+            this.retryHint = Preconditions.checkNotNull(value, "retryHint");
+            return this;
+        }
+
+        public Builder retryHint(Optional<RetryHint> value) {
+            this.retryHint = Preconditions.checkNotNull(value, "retryHint").orElse(null);
+            return this;
+        }
+
+        public Builder dueTo(DueTo value) {
+            this.dueTo = Preconditions.checkNotNull(value, "dueTo");
+            return this;
+        }
+
+        public Builder dueTo(Optional<DueTo> value) {
+            this.dueTo = Preconditions.checkNotNull(value, "dueTo").orElse(null);
+            return this;
+        }
+
+        public QosReason build() {
+            return new QosReason(
+                    Preconditions.checkNotNull(reason, "reason"),
+                    Optional.ofNullable(retryHint),
+                    Optional.ofNullable(dueTo));
+        }
+    }
+
+    public enum RetryHint {
+        // TODO(ckozak): Should we declare the existing default value? Perhaps best if we only
+        // declare the minimal api to opt into new behavior.
+        //        /** Clients should attempt to retry this failure. */
+        //        EAGER,
+        /**
+         * Clients should not attempt to retry this failure,
+         * providing the failure as context back to the initial caller.
+         */
+        PROPAGATE;
+    }
+
+    public enum DueTo {
+        // TODO(ckozak): should we declare values that align with the default 503 and 429 behavior, which
+        // impact rate limiters for the node and endpoint specifically? If we do, they will be propagated
+        // in ways that don't make sense (despite being the default behavior today). Declaring a user limit
+        // may be more reasonable since the user token is generally passed along, however per-endpoint limits
+        // lose some meaning when rethrown by a calling service.
+
+        /**
+         * A cause that the RPC system isn't directly aware of, for example a user or user-agent specific limit, or
+         * based on a specific resource that's being accessed, as opposed to the target node as a whole, or endpoint.
+         * QosReasons with this cause shouldn't impact things like the dialogue concurrency limiter.
+         */
+        CUSTOM;
+    }
+
+    private static void checkReason(@Safe String reason) {
+        if (reason == null || reason.isEmpty() || reason.length() > 50) {
+            throw invalidReason(reason);
+        }
+        for (int i = 0; i < reason.length(); i++) {
+            char character = reason.charAt(i);
+            boolean validCharacter = (character >= 'a' && character <= 'z')
+                    || (character >= '0' && character <= '9')
+                    || character == '-';
+            if (!validCharacter) {
+                throw invalidReason(reason);
+            }
+        }
+    }
+
+    @CheckReturnValue
+    private static SafeIllegalArgumentException invalidReason(@Safe String reason) {
+        return new SafeIllegalArgumentException(
+                "Reason must be at most 50 characters, and only contain lowercase letters, numbers, "
+                        + "and hyphens (-).",
+                SafeArg.of("reason", reason));
     }
 }
---
+++ errors/src/main/java/com/palantir/conjure/java/api/errors/QosReasons.java
@@ -0,0 +1,60 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.conjure.java.api.errors;
+
+import com.palantir.conjure.java.api.errors.QosReason.DueTo;
+import com.palantir.conjure.java.api.errors.QosReason.RetryHint;
+import java.util.Optional;
+
+public final class QosReasons {
+
+    private static final String DUE_TO_HEADER = "Qos-Due-To";
+    private static final String RETRY_HINT_HEADER = "Qos-Retry-Hint";
+
+    public <T> void writeToResponse(QosReason reason, T response, QosResponseEncodingAdapter<? super T> adapter) {
+        reason.dueTo().ifPresent(dueTo -> adapter.setHeader(response, DUE_TO_HEADER, dueTo.name()));
+        reason.retryHint().ifPresent(retryHint -> adapter.setHeader(response, RETRY_HINT_HEADER, retryHint.name()));
+    }
+
+    public <T> QosReason parseFromResponse(
+            QosReason defaultReason, T response, QosResponseDecodingAdapter<? super T> adapter) {
+        return QosReason.builder()
+                .from(defaultReason)
+                .dueTo(adapter.getFirstHeader(response, DUE_TO_HEADER).flatMap(value -> parse(DueTo.class, value)))
+                .retryHint(adapter.getFirstHeader(response, RETRY_HINT_HEADER)
+                        .flatMap(value -> parse(RetryHint.class, value)))
+                .build();
+    }
+
+    public interface QosResponseEncodingAdapter<RESPONSE> {
+        void setHeader(RESPONSE response, String headerName, String headerValue);
+    }
+
+    public interface QosResponseDecodingAdapter<RESPONSE> {
+        Optional<String> getFirstHeader(RESPONSE response, String headerName);
+    }
+
+    private <T extends Enum<T>> Optional<T> parse(Class<T> type, String value) {
+        try {
+            return Optional.of(Enum.valueOf(type, value));
+        } catch (IllegalArgumentException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    private QosReasons() {}
+}
