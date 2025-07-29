c920e81bb18a9a62c100fe48ddd394eb8151cc2a
Pritham Marupaka <mpritham@users.noreply.github.com>
2025-07-29 13:38:11 -0400

    Add `ConjureErrorParameterFormat` with utility to encode and parse HTTP header (#1398)
    Add `ConjureErrorParameterFormat` with utility to encode and parse HTTP header

---
+++ errors/src/main/java/com/palantir/conjure/java/api/errors/ConjureErrorParameterFormats.java
@@ -0,0 +1,89 @@
+/*
+ * (c) Copyright 2025 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.conjure.java.api.errors;
+
+import com.palantir.logsafe.Preconditions;
+import java.util.Optional;
+
+/**
+ * Utility methods for encoding and decoding the expected format for Conjure error parameters in HTTP requests.
+ *
+ * <p>This class provides methods for clients to set their expected format for receiving error parameters
+ * from servers, and for servers to parse these preferences from incoming requests.
+ *
+ * <p>The format is transmitted via the {@value #ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER} HTTP header.
+ */
+public final class ConjureErrorParameterFormats {
+    // The HTTP header name used to specify the expected Conjure error parameter format.
+    // VisibleForTesting
+    static final String ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER = "Accept-Conjure-Error-Parameter-Format";
+
+    /**
+     * Encodes the error parameter format into an outgoing request via HTTP header.
+     *
+     * <p>This method sets the {@value #ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER} header on the
+     * provided request object using the supplied adapter.
+     *
+     * @param <T> the type of request object
+     * @param format the format for error parameters
+     * @param request the request object to set headers on
+     * @param adapter the adapter for setting headers on the request
+     */
+    public static <T> void encodeToRequest(
+            ConjureErrorParameterFormat format,
+            T request,
+            ConjureErrorParameterFormatRequestEncodingAdapter<? super T> adapter) {
+        Preconditions.checkNotNull(format, "Conjure error parameter format is required");
+        Preconditions.checkNotNull(request, "Request is required");
+        Preconditions.checkNotNull(adapter, "Adapter is required");
+
+        adapter.setHeader(request, ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER, format.toString());
+    }
+
+    /**
+     * Parses the error parameter format from an incoming request's HTTP headers.
+     *
+     * <p>This method reads the {@value #ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER} header from the
+     * provided request object using the supplied adapter.
+     *
+     * @param <T> the type of request object
+     * @param request the request object to read headers from
+     * @param adapter the adapter for reading headers from the request
+     * @return the format for error parameters, or empty if not specified
+     */
+    public static <T> Optional<ConjureErrorParameterFormat> parseFromRequest(
+            T request, ConjureErrorParameterFormatRequestDecodingAdapter<? super T> adapter) {
+        Preconditions.checkNotNull(request, "Request is required");
+        Preconditions.checkNotNull(adapter, "Adapter is required");
+
+        String headerValue = adapter.getFirstHeader(request, ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER);
+        if (headerValue == null || headerValue.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(ConjureErrorParameterFormat.valueOf(headerValue));
+    }
+
+    public interface ConjureErrorParameterFormatRequestEncodingAdapter<REQUEST> {
+        void setHeader(REQUEST response, String headerName, String headerValue);
+    }
+
+    public interface ConjureErrorParameterFormatRequestDecodingAdapter<REQUEST> {
+        String getFirstHeader(REQUEST response, String headerName);
+    }
+
+    private ConjureErrorParameterFormats() {}
+}
---
+++ errors/src/test/java/com/palantir/conjure/java/api/errors/ConjureErrorParameterFormatsTest.java
@@ -0,0 +1,130 @@
+/*
+ * (c) Copyright 2025 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.conjure.java.api.errors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.palantir.conjure.java.api.errors.ConjureErrorParameterFormats.ConjureErrorParameterFormatRequestDecodingAdapter;
+import com.palantir.conjure.java.api.errors.ConjureErrorParameterFormats.ConjureErrorParameterFormatRequestEncodingAdapter;
+import com.palantir.logsafe.exceptions.SafeNullPointerException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+public final class ConjureErrorParameterFormatsTest {
+
+    @Test
+    public void encodeToRequest_setsHeaderCorrectly() {
+        Map<String, String> request = new HashMap<>();
+        ConjureErrorParameterFormat format = ConjureErrorParameterFormat.JSON_FORMAT;
+        ConjureErrorParameterFormats.encodeToRequest(format, request, Encoder.INSTANCE);
+        assertThat(request)
+                .containsEntry(ConjureErrorParameterFormats.ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER, "JSON");
+    }
+
+    @Test
+    public void encodeToRequest_null_checks() {
+        Map<String, String> request = new HashMap<>();
+        assertThatThrownBy(() -> ConjureErrorParameterFormats.encodeToRequest(null, request, Encoder.INSTANCE))
+                .isInstanceOf(SafeNullPointerException.class)
+                .hasMessage("Conjure error parameter format is required");
+
+        ConjureErrorParameterFormat format = ConjureErrorParameterFormat.JSON_FORMAT;
+        assertThatThrownBy(() -> ConjureErrorParameterFormats.encodeToRequest(format, null, Encoder.INSTANCE))
+                .isInstanceOf(SafeNullPointerException.class)
+                .hasMessage("Request is required");
+
+        assertThatThrownBy(() -> ConjureErrorParameterFormats.encodeToRequest(format, request, null))
+                .isInstanceOf(SafeNullPointerException.class)
+                .hasMessage("Adapter is required");
+    }
+
+    @Test
+    public void parseFromRequest_headerPresent_returnsFormat() {
+        Map<String, String> request = new HashMap<>();
+        request.put(ConjureErrorParameterFormats.ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER, "JSON");
+        assertThat(ConjureErrorParameterFormats.parseFromRequest(request, Decoder.INSTANCE))
+                .containsSame(ConjureErrorParameterFormat.JSON_FORMAT);
+    }
+
+    @Test
+    public void parseFromRequest_headerMissing_returnsEmpty() {
+        Map<String, String> request = new HashMap<>();
+        assertThat(ConjureErrorParameterFormats.parseFromRequest(request, Decoder.INSTANCE))
+                .isEmpty();
+    }
+
+    @Test
+    public void parseFromRequest_headerEmpty_returnsEmpty() {
+        Map<String, String> request = new HashMap<>();
+        request.put(ConjureErrorParameterFormats.ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER, "");
+        assertThat(ConjureErrorParameterFormats.parseFromRequest(request, Decoder.INSTANCE))
+                .isEmpty();
+    }
+
+    @Test
+    public void parseFromRequest_null_checks() {
+        assertThatThrownBy(() -> ConjureErrorParameterFormats.parseFromRequest(null, Decoder.INSTANCE))
+                .isInstanceOf(SafeNullPointerException.class)
+                .hasMessage("Request is required");
+
+        Map<String, String> request = new HashMap<>();
+        assertThatThrownBy(() -> ConjureErrorParameterFormats.parseFromRequest(request, null))
+                .isInstanceOf(SafeNullPointerException.class)
+                .hasMessage("Adapter is required");
+    }
+
+    @Test
+    public void parseFromRequest_customFormat_returnsCorrectFormat() {
+        Map<String, String> request = new HashMap<>();
+        request.put(ConjureErrorParameterFormats.ACCEPT_CONJURE_ERROR_PARAMETER_FORMAT_HEADER, "CUSTOM_FORMAT");
+        Optional<ConjureErrorParameterFormat> result =
+                ConjureErrorParameterFormats.parseFromRequest(request, Decoder.INSTANCE);
+        assertThat(result).isPresent();
+        assertThat(result.get().toString()).isEqualTo("CUSTOM_FORMAT");
+    }
+
+    @Test
+    public void roundTrip() {
+        Map<String, String> request = new HashMap<>();
+        ConjureErrorParameterFormat originalFormat = ConjureErrorParameterFormat.JSON_FORMAT;
+        ConjureErrorParameterFormats.encodeToRequest(originalFormat, request, Encoder.INSTANCE);
+        Optional<ConjureErrorParameterFormat> parsedFormat =
+                ConjureErrorParameterFormats.parseFromRequest(request, Decoder.INSTANCE);
+        assertThat(parsedFormat).containsSame(originalFormat);
+    }
+
+    private enum Encoder implements ConjureErrorParameterFormatRequestEncodingAdapter<Map<String, String>> {
+        INSTANCE;
+
+        @Override
+        public void setHeader(Map<String, String> request, String headerName, String headerValue) {
+            request.put(headerName, headerValue);
+        }
+    }
+
+    private enum Decoder implements ConjureErrorParameterFormatRequestDecodingAdapter<Map<String, String>> {
+        INSTANCE;
+
+        @Override
+        public String getFirstHeader(Map<String, String> request, String headerName) {
+            return request.get(headerName);
+        }
+    }
+}
